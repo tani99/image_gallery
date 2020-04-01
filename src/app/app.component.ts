e643e3f27d8b008175aa12ed69147cf2f7638a96
Tanisha <tanisha.mandre@kcl.ac.uk>
2020-04-01 18:59:02 +0100

    Image uploading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { map } from 'rxjs/operators/map';
 import {
   AngularFireStorage,
   AngularFireStorageReference,
   AngularFireUploadTask
 } from 'angularfire2/storage';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +17,9 @@ export class AppComponent {
   title = 'image-gallery';
   ref: AngularFireStorageReference;
   task: AngularFireUploadTask;
-
+  uploadProgress: Observable<number>;
+  downloadURL: Observable<string>;
+  uploadState: Observable<string>;
   constructor(private afStorage: AngularFireStorage) {}
 
   upload(event) {
@@ -24,5 +28,12 @@ export class AppComponent {
       .substring(2);
     this.ref = this.afStorage.ref(id);
     this.task = this.ref.put(event.target.files[0]);
+    // this.uploadState = this.task.snapshotChanges().pipe(map(s => s.state));
+    this.uploadProgress = this.task.percentageChanges();
+    // this.downloadURL = this.ref.getDownloadURL();
+  }
+
+  getUserProfileImage(request: FirebaseRequest) {
+    return 'images/users/' + request.userId + '_users.jpg';
   }
 }
