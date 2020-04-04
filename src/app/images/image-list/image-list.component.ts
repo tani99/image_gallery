09c5e2f12af6d8ad28d996099492750ffc9e2930
Tanisha <tanisha.mandre@kcl.ac.uk>
2020-04-04 09:42:28 +0100

    Displaying image gallery

--- src/app/images/image-list/image-list.component.ts
+++ src/app/images/image-list/image-list.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from 'src/app/shared/image.service';
 
 @Component({
   selector: 'app-image-list',
   templateUrl: './image-list.component.html'
 })
 export class ImageListComponent implements OnInit {
-  constructor() {}
+  imageList: any[];
+  rawIndexArray: any[];
+  constructor(private service: ImageService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.service.imageDetailList.snapshotChanges().subscribe(list => {
+      this.imageList = list.map(item => {
+        return item.payload.val();
+      });
+      this.rawIndexArray = Array.from(
+        // To set number of rows
+        Array(Math.ceil((this.imageList.length + 1) / 3)).keys()
+      );
+    });
+  }
 }
